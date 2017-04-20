b641daa180885624827c8ab1283c19c5cc9a3194
Tobias <cchawps@gmail.com>
2017-04-20 12:11:53 +0200

    map added

--- js/properties.single.js
+++ js/properties.single.js
@@ -63,6 +63,9 @@ Properties.single = function (id) {
                 <span class="ml-auto">${data.price} kr.</span>
             </li>
         </ul>
+        <div class="card-block">
+            <iframe src="${data.map}" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
         `);
     });
 }
